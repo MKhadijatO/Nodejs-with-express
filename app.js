9704388351427e406ed4d93a475c4ee7190b987b
Khadijat <muhammadkhadijato@gmail.com>
2023-08-18 22:32:47 +0100

    Working with express js: sending a text response

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
+// IMPORT PACKAGE
+
+const express = require ('express');
+let app = express();
+
+// ROUTE = HTTP METHOD + URL
+
+app.get('/', (req, res) => {
+    res.status(200).send('Hello from express server');
+})
+
+
+
+// CREATE A SERVER
+const port = 3000;
+app.listen(port, ()=> {
+    console.log('server has started...')
+});
+
 
