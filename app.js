4893e28fff37a219187df3bcac05c30eb28b2505
Khadijat <muhammadkhadijato@gmail.com>
2023-08-18 23:20:56 +0100

    Working with express js: sending a html response in a get request

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
+const fs = require('fs');
+
+
 // IMPORT PACKAGE
 const express = require ('express');
 let app = express();
 
 // ROUTE = HTTP METHOD + URL
+const html = fs.readFileSync('./Files/index.html', 'utf-8');
 
 app.get('/', (req, res) => {
-    res.status(200).json({greeting: 'Hello, friend', message: 'This is just an example'});
+    res.status(200).send(html);
 })
 
 //post request
-app.post('/', () => {
+// app.post('/', () => {
    
-})
+// })
 
 
 
