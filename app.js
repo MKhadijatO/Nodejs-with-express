45cb5bcbb8234e0f6fe937c85f8e8af1d6911a2f
Khadijat <muhammadkhadijato@gmail.com>
2023-08-24 00:56:31 +0100

    creating and using routes module 03

--- app.js
+++ app.js
@@ -2,48 +2,32 @@
 
 const express = require('express');
 const morgan = require('morgan');
-
 const moviesRouter = require('./Routes/moviesRoutes');
 
 
 let app = express();
-
-
 const logger = function(req, res, next) {
     console.log ('Custom middleware called'); 
     next();
 }
 
+
+
 app.use(express.json());
 app.use(morgan('tiny'));
 app.use(logger);
-app.use((req, res, next) => {
+app.use((req, res, next) => { 
     req.requestedAt = new Date().toISOString();
     next();
 });
 
 
 
-
-
-
-// ********* APIs ********
-// app.get('/api/v1/movies', getAllMovies);
-// app.get('/api/v1/movies/:id', getMovie);
-// app.post('/api/v1/movies', createMovie);
-// app.patch('/api/v1/movies/:id', updateMovie);
-// app.delete('/api/v1/movies/:id', deleteMovie)
-
-
-
+// USING ROUTES
 app.use('/api/v1/movies', moviesRouter)
 
 
+module.exports = app;
 
-// CREATE A SERVER
-const port = 3000;
-app.listen(port, ()=> {
-    console.log('server has started...')
-});
 
 
---
+++ server.js
@@ -0,0 +1,12 @@
+const app = require('./app');
+
+
+
+
+// CREATE A SERVER
+
+const port = 3000;
+
+app.listen(port, () => {
+    console.log('server has started...');
+})
