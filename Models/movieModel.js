ef59fbcfc0fca993c581c8b122bf50088cfa9df0
Khadijat <muhammadkhadijato@gmail.com>
2023-08-31 00:31:46 +0100

    updated movie schema

--- Models/movieModel.js
+++ Models/movieModel.js
@@ -3,17 +3,56 @@ const movieSchema = new mongoose.Schema({
     name: {
         type: String,
         required: [true, 'Name is required'],
-        unique: true
+        unique: true,
+        trim: true
     },
-    description: String,
+    description: {
+        type: String,
+        required: [true, 'Description is required'],
+        trim: true
+    },
     duration: {
         type: Number,
         required: [true, 'Duration is required']
     },
     ratings: {
         type: Number,
-        default: 1.0
+    },
+    totalRating:{
+        type: Number,
+    },
+    releaseYear: {
+        type: Number,
+        required: [true, 'Release year is required']
+    },
+    releaseDate:{
+        type: Date,
+    },
+    createdAt:{
+        type: Date,
+        default: Date.now
+    },
+    genre: {
+        type: [String],
+        required: [true, 'Genres is required']
+    }, 
+    directors: {
+        type: [String],
+        required: [true, 'Directors is required']
+    },
+    coverImage: {
+        type: String,
+        require: [true, 'Cover image is required']
+    },
+    actors: {
+        type: [String],
+        required: [true, 'actors is required']
+    },
+    price: {
+        type: Number,
+        require: [true, 'Price is required']
     }
+
 });
 
 const Movie = mongoose.model('Movie', movieSchema);
