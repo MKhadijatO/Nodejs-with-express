a9c43b9544ea9dbb9a99404fcbbe231d2e15b15f
Khadijat <muhammadkhadijato@gmail.com>
2023-08-30 09:41:15 +0100

    connecting db to nodejs app with  mongoose

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config({path: './config.env'});
 
@@ -6,6 +7,14 @@ const app = require('./app');
 //console.log(app.get('env'));
 console.log(process.env);
 
+mongoose.connect(process.env.CONN_STR, {
+    useNewUrlParser: true
+}).then((conn) => {
+    //console.log(conn);
+    console.log('DB connection successful');
+}).catch((error) => {
+    console.log('Some error occurred');
+}); 
 
 // CREATE A SERVER
 
