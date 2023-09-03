d8f0b0ec89626b8849012ae6b803b978e82013aa
Khadijat <muhammadkhadijato@gmail.com>
2023-09-03 21:50:55 +0100

    error msg in dev. env and prod. env

--- Controllers/errorController.js
+++ Controllers/errorController.js
@@ -1,9 +1,21 @@
 module.exports = (error, req, res, next) => {
     error.statusCode = error.statusCode || 500;
     error.status = error.status || 'error';
-    res.status(error.statusCode).json({
-        status: error.statusCode,
-        message:error.message
-    });
+
+    if (process.env.NODE_ENV === 'development') {
+        res.status(error.statusCode).json({
+            status: error.statusCode,
+            message:error.message,
+            stackTrace: error.stack,
+            errors: error
+        });
+    } else if(process.env.NODE_ENV === 'production'){
+        res.status(error.statusCode).json({
+            status: error.statusCode,
+            message:error.message
+        });
+    }
+
+    
 
 }
